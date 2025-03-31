6fe9b338b9bdd7eeb0032019b071ad58b8c8c7ca
Onur-Han-Kagan <onurhanturkbayragi@gmail.com>
2025-03-31 23:40:30 +0300

    İlk commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rise Upgrade Assistant</h1>
+      <p>React projesi başarıyla kuruldu.</p>
+    </div>
+  );
+}
+
+export default App;
